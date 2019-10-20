4b6414522775b0bdbe8fd0e5619a87506c80dcda
Dennis <denniskobberholm3@gmail.com>
2019-10-20 14:30:35 +0200

    Miw did something. i reverted it back to the original. I think..

--- OtherMirror/app/src/main/java/com/example/othermirror/MirrorSettings.java
+++ OtherMirror/app/src/main/java/com/example/othermirror/MirrorSettings.java
@@ -1,10 +1,14 @@
 package com.example.othermirror;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +18,23 @@ public class MirrorSettings extends AppCompatActivity {
 
     Button cancel;
     Button done;
-    ImageView mirrorSettings;
+    WebView mirrorSettings;
+
+    public class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context
+         * @param c*/
+        WebAppInterface(MirrorSettings c) {
+            mContext = c;
+        }
+
+        /** Show a toast from the web page */
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +44,13 @@ public class MirrorSettings extends AppCompatActivity {
         setContentView(R.layout.activity_mirror_settings);
         cancel = (Button) findViewById(R.id.cancel_mirror_settings);
         done = (Button) findViewById(R.id.done_mirror_settings);
-        mirrorSettings = (ImageView) findViewById(R.id.change_mirror_settings);
+        mirrorSettings = (WebView) findViewById(R.id.miwswebview);
+        mirrorSettings.getSettings().setJavaScriptEnabled(true);
+        mirrorSettings.loadUrl("file:///android_asset/MobDesigner.html"); //so design me maybe, and all the other girls, this is crazy
+
+        mirrorSettings.addJavascriptInterface(new WebAppInterface(this), "Android");
+
+
 
         done.setOnClickListener(new View.OnClickListener() {
             @Override
